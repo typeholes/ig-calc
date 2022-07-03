58d472cfe3c84ab9639a625bd4659b9ffa5a58b1
typeholes <typeholes@protonmail.com>
2022-07-03 15:27:38 -0400

    fixing eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,26 @@
 module.exports = {
-    env: {
-       browser: true,
-       es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    parserOptions: {
-       ecmaVersion: 'latest',
-       sourceType: 'module',
+    "extends": [
+//        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-strongly-recommended",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
     },
-    extends: ['eslint:recommended'],
-   rules: { 'no-var': 'error' },
- };
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+//        "no-explicit-any": "off",
+//        "@typescript-eslint/ban-types": [ "error", { types: { string: 'temp error until VarNames and MilestoneNames are used'}}]
+        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" , "varsIgnorePattern": "^_" } ]
+    }
+}
