4ae7d1ab2bbbd1ad26496a306c0e78b65740c5de
typeholes <typeholes@protonmail.com>
2022-07-03 18:53:14 -0400

    more actions

--- src/js/actions.ts
+++ src/js/actions.ts
@@ -7,6 +7,7 @@ import {
    state as appState,
 } from '../components/uiUtil';
 import { cursorState, goToElement } from '../components/FakeCursor';
+import { nextTick } from 'vue';
 
 interface ActionArgs {
    args: string[];
@@ -14,22 +15,90 @@ interface ActionArgs {
 }
 type Action = [string, ActionArgs];
 
+let selectedElement: HTMLElement | undefined = undefined;
+
 const actionHandlers: Record<string, (...x: string[]) => void> = {
+   wait: () => {},
    alert: alert,
    setNewExpr: (x) => {
       appState.newExpr = x;
       checkNewExpr();
    },
-   addExpr: (x) => {
-      appState.newExpr = x;
-      checkNewExpr();
-      addToEnv(x, true);
+   addExpr: (expr, short) => {
+      if (short === 'short') {
+         appState.newExpr = expr;
+         checkNewExpr();
+         addToEnv(expr, true);
+      } else {
+         // TODO: function to append multiple actions so we don't have to do them in reverse order
+         prependAction('wait', 2);
+         prependAction('click', 0.5);
+         prependAction('wait', 2);
+         prependAction('goto', 2.5, 'addExpr');
+         prependAction('wait', 2);
+         prependAction('type', 1, expr);
+         prependAction('wait', 2);
+         prependAction('click', 0.5);
+         prependAction('wait', 2);
+         prependAction('goto', 2.5, 'newExpr');
+         prependAction('wait', 2);
+      }
    },
    showCursor: () => {
       cursorState.show = true;
    },
-   goto: goToElement,
+   select: (x) => {
+      if (defined(x)) {
+         selectedElement = document.getElementById(x) ?? undefined;
+      }
+   },
+   goto: (x) => {
+      actionHandlers.select(x);
+      goToElement(selectedElement);
+   },
+   click: (x) => {
+      actionHandlers.select(x);
+      selectedElement?.click();
+   },
+   set: (value, elementId) => {
+      actionHandlers.select(elementId);
+      if (selectedElement instanceof HTMLInputElement) {
+         selectedElement.value = value;
+         selectedElement.dispatchEvent(new Event('change', { bubbles: true }));
+      }
+   },
+   append: (value, elementId) => {
+      actionHandlers.select(elementId);
+      if (selectedElement instanceof HTMLInputElement) {
+         selectedElement.value += value;
+         selectedElement.dispatchEvent(new Event('input', { bubbles: true }));
+      }
+   },
+   type: (value, elementId) => {
+      actionHandlers.select(elementId);
+      if (selectedElement instanceof HTMLInputElement) {
+         actions?.unshift(
+            ...value.split('').map((char) => mkAction('append', 0.2, [char]))
+         );
+      }
+   },
 };
+
+function mkAction(
+   name: keyof typeof actionHandlers,
+   delay: number,
+   args: string[]
+): Action {
+   return [name, { delay, args }];
+}
+
+function prependAction(
+   name: keyof typeof actionHandlers,
+   delay: number,
+   ...args: string[]
+) {
+   actions?.unshift([name, { delay, args }]);
+}
 
 export function runAction([name, { delay, args }]: Action) {
    if (name in actionHandlers) {
@@ -81,7 +150,14 @@ export function tick(elapsedSeconds: number) {
 
    if (defined(actions) && actions.length > 0) {
       const action = actions.shift()!;
-      runAction(action);
       nextTime += action[1].delay;
+      console.log('running action', {
+         time,
+         nextTime,
+         name: action[0],
+         ...action[1],
+         selected: selectedElement?.id,
+      });
+      runAction(action);
    }
 }
