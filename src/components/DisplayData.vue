7166fd867bbcf400807c4d67a3e7dcf2bfb9cad1
typeholes <typeholes@protonmail.com>
2022-08-03 01:50:42 -0400

    styling for data grid

--- src/components/DisplayData.vue
+++ src/components/DisplayData.vue
@@ -1,103 +1,101 @@
 <script setup lang="ts">
-   import CanError from './CanError.vue';
-   import { errorable } from '../js/Either';
-   import { arrayRange } from '../js/function-plot/utils';
-   import { EnvItem } from '../js/env/exprEnv';
-   import { defined } from '../js/util';
-   import { Map as IMap } from 'immutable';
-   import { state as appState } from './uiUtil';
-import { computed } from 'vue';
-import { currentEnv } from 'src/components/SaveWidget'
+import { Errorable, errorable } from '../js/Either';
+import { EnvItem } from '../js/env/exprEnv';
+import { defined, isString } from '../js/util';
+import { Map as IMap } from 'immutable';
+import { computed, reactive } from 'vue';
+import { currentEnv } from 'src/components/SaveWidget';
+import { arrayRange } from 'src/js/function-plot/utils';
 
-   function getNames() {
-      const names =  IMap(currentEnv.value.items).filter ( (item) => item.showGraph);
-      return names;
-   }
+function getNames() {
+  const names = IMap(currentEnv.value.items).filter((item) => item.showGraph);
+  return names;
+}
 
-const names = computed(getNames);
+const names = computed(() => getNames().filter((item) => item.showGraph));
 
-   function formatName(name: string) {
-      if (name === '__tmp') {
-         return 'temp';
+function formatNumber(n: number | string | undefined /*row: number*/) {
+  if (isString(n)) {
+    return n;
+  }
+
+  return defined(n) ? n.toExponential(3) : '';
+}
+
+function runFn(name: string, item: EnvItem, n: number) {
+  return errorable(() => {
+    const datum = currentEnv.value.getDatum(name);
+    if (defined(datum) && datum.evalFn instanceof Function) {
+      return datum.evalFn(n);
+    }
+    return 0;
+  });
+}
+
+const columns = computed(() => [
+  {
+    name: 'free',
+    label: 'free',
+    sortable: true,
+    field: 'free',
+    style: `border: 1px solid white`,
+    headerStyle: `border: 1px solid white`,
+    required: true,
+    format: formatNumber,
+    align: 'right',
+  } as const,
+  ...names.value
+    .map(
+      (item, name) =>
+        ({
+          name,
+          label: name,
+          sortable: true,
+          field: name,
+          style: `border: 1px solid ${item.color}`,
+          headerStyle: `border: 1px solid ${item.color}`,
+          required: false,
+          format: formatNumber,
+          align: 'right',
+        } as const)
+    )
+    .values(),
+]);
+
+function rows(freeValues: number[]) {
+  return freeValues.map((x) => {
+    const ret = { free: x } as Record<string, string | number>;
+    names.value.forEach((item) => {
+      if (!defined(item)) {
+        return;
       }
-      return name.replace(/^anon: /, '');
-   }
-
-   function formatNumber(n: number) {
-      return n; // TODO
-   }
-
-   function runFn(name: string, item: EnvItem, n: number) {
-      return errorable(() => {
-         const datum = currentEnv.value.getDatum(name);
-         if (defined(datum) && datum.evalFn instanceof Function) {
-            return formatNumber(datum.evalFn(n));
-         }
-         return '';
+      ret[item.name] = Errorable.on(runFn(item.name, item, x), {
+        Left: (e) => e.message as string | number,
+        Right: (n) => n,
       });
-   }
+    });
+    return ret;
+  });
 
+}
+
+// eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+const freeValues = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 987654321.0123456789].concat(
+  arrayRange(0, 120)
+);
 </script>
 
-<template >
-   <div class="fillLeft lastSmall">
-      <div>&nbsp;</div>
-      <div
-         class="dataGrid"
-         :style="{
-            gridTemplateColumns: `repeat( ${names.size + 2}, auto)`,
-         }"
-         :key="currentEnv.constant.get('time')"
-      >
-         <div style="gridcolumn: 1">free</div>
-         <template :key="name" v-for="([name, item], idx) of names">
-            <div
-               :style="{
-                  gridColumn: idx + 2,
-                  border: `1px solid ${item.color ?? 'white'}`,
-               }"
-            >
-               {{ formatName(name) }}
-            </div>
-         </template>
-         <template
-            :key="num"
-            v-for="(num, rowNum) in arrayRange(
-               appState.freeMin,
-               appState.freeMax
-            )"
-         >
-            <div :style="{ gridColumn: 1, gridRow: rowNum + 2 }">{{ num }}</div>
-            <template :key="name" v-for="([name, item], idx) of names">
-               <div
-                  :style="{
-                     gridColumn: idx + 2,
-                     border: `1px solid ${item.color ?? 'white'}`,
-                  }"
-               >
-                  <CanError
-                     :value="runFn(name, item, num)"
-                  ></CanError>
-               </div>
-            </template>
-         </template>
-      </div>
-   </div>
+<template>
+  <div class="fixed-center q-pa-md">
+    <!-- <q-scroll-area id="data-grid-scroll-area"> -->
+    <q-table
+      :rows="rows(freeValues)"
+      :columns="columns"
+      row-key="free"
+      virtual-scroll
+      dense
+      style="height: 80vh"
+    />
+    <!-- </q-scroll-area> -->
+  </div>
 </template>
-
-<style>
-
-   .fillLeft {
-      display: flex;
-      width: 90vw;
-   }
-
-   .dataGrid {
-      display: grid;
-      border: 1px solid whitesmoke;
-      column-gap: 2px;
-      overflow: auto auto;
-      max-width: 60vw;
-      margin-right: 20vw;
-   }
-</style>
