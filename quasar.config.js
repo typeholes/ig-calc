328d967b8f2bab817ccde53958881c2366694456
typeholes <typeholes@protonmail.com>
2022-10-13 19:47:19 -0400

    do not minify prod build

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (/* ctx */) {
       // env: {},
       // rawDefine: {}
       // ignorePublicFolder: true,
-      // minify: false,
+      minify: false,
       // polyfillModulePreload: true,
       // distDir
 
